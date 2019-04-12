673ce38737aff7f708a57e8a0f04ad85e5958249
Jack Volontyr <soapforwork@gmail.com>
2019-04-12 16:40:22 +0300

    Create basic components.

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -1,11 +1,2 @@
 module RoutesHelper
-  def trains_on(route)
-    route.trains.any? ?
-        route.trains.map(&:number).join(', ') : "------"
-  end
-
-  def stations_on(route)
-    route.stations.any? ?
-        route.stations.map(&:name).join(', ') : "------"
-  end
 end
