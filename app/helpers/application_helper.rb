997b52bde874ad13c687cc327432bf71465420e2
Jack Volontyr <soapforwork@gmail.com>
2019-04-24 22:59:30 +0300

    Update `application_helper` for `get_caption`.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,28 @@ module ApplicationHelper
   def table_cell_with
     content_tag :div, yield, class: "tutu-table__cell"
   end
+
+  def get_caption(for_item, type)
+    case for_item
+    when "carriages"
+      for_item = ["Номер", "Тип Вагона", "Н Места", "В Места", "НБ Места", "ВБ Места", "Места", "Поезда"]
+    when "routes"
+      for_item = ["Имя", "Поезд", "Станции"]
+    when "stations"
+      for_item = ["Имя", "Пути", "Билеты"]
+    else
+      for_item = []
+    end
+
+    case type
+    when "index"
+      ["№"] + for_item + ["", "", ""]
+    when "show"
+      for_item + ["", "", ""]
+    when "form"
+      for_item
+    else
+      []
+    end
+  end
 end
