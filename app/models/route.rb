6ee03157f255cf0b2ac8905c8a26cfc97faa4283
Jack Volontyr <soapforwork@gmail.com>
2019-04-30 19:46:42 +0300

    Auto naming for `route` model.

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,4 +4,24 @@ class Route < ApplicationRecord
   has_many :trains
   has_many :ways
   has_many :stations, through: :ways
+
+  before_validation :auto_naming
+
+  protected
+
+  def auto_naming
+    if stations.present?
+      self[:name] = "#{get_first_station.name} -> #{get_last_station.name}"
+    else
+      self[:name] = "Станции не выбраны"
+    end
+  end
+
+  def get_first_station
+    stations.ordered.first
+  end
+
+  def get_last_station
+    stations.ordered.last
+  end
 end
