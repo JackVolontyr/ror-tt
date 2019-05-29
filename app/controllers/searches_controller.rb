3a5f9567e0828a3fe6b242d2e1e70441d5933602
Jack Volontyr <soapforwork@gmail.com>
2019-05-29 14:20:27 +0300

    Create `searches_controller`.

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,11 @@
+class SearchesController < ApplicationController
+  def show
+    stations = Route.joins(:stations)
+    station_first = params[:station_first]
+    station_last = params[:station_last]
+
+    if station_first.present? && station_last.present?
+      @routes = stations.where(stations: { name: [station_first, station_last] }).uniq
+    end
+  end
+end
