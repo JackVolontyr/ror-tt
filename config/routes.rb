327cc4430568052ceec7179fdb9fbe14c7792cfa
Jack Volontyr <soapforwork@gmail.com>
2019-06-06 15:21:25 +0300

    Admin interface: `routes` changes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,27 @@
 Rails.application.routes.draw do
-  resources :trains do
-    resources :carriages
-  end
-
   devise_for :users
+
   resources :users do
     resources :tickets
   end
 
-  resources :stations do
-    patch :update_position, on: :member
-    patch :update_arrival, on: :member
-    patch :update_departure, on: :member
-  end
-
-  resources :routes, :services, :seats, :welcome
+  resources :welcome
 
   resource :search, only: %i[new show edit]
+
+  namespace :admin do
+    resources :trains do
+      resources :carriages
+    end
+
+    resources :stations do
+      patch :update_position, on: :member
+      patch :update_arrival, on: :member
+      patch :update_departure, on: :member
+    end
+
+    resources :routes, :services, :seats, :tickets
+  end
 
   get 'searches/show'
   root 'searches#show'
