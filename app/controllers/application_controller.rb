312d20a15b020b1d92f818287e135693442f7752
Jack Volontyr <soapforwork@gmail.com>
2019-06-06 18:05:54 +0300

    Admin interface: update views and `application_controller`.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    searches_show_path
+    current_user.admin? ? welcome_index_path : searches_show_path
   end
 
   def after_sign_out_path_for(resource)
