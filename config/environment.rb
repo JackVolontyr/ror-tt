58d7224a8b4de3df338d40e6a80b7ee44915c2e0
JackVolontyr <soapforwork@gmail.com>
2019-06-26 12:06:46 +0300

    Replace `smtp` to `production`.

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-    :user_name => 'app136830725@heroku.com',
-    :password => '5aasjxab3869',
-    :domain => 'tutu-just.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-}
