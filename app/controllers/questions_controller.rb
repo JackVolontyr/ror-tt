4f9f12f173dd9f606cb2935f11e8000652ded0bc
JackVolontyr <soapforwork@gmail.com>
2019-07-08 15:16:55 +0300

    Update `questions_controller` (has TODO).

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,43 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+
   def index
     @questions = Question.all
+  end
+
+  def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+  end
+
+  def create
+    # TODO: nested :questions to user
+    @question = Question.new question_params
+    @question.save ?
+        redirect_to(@question) : render(:new)
+  end
+
+  def update
+    @question.update(question_params) ?
+        redirect_to(@question) : render(:edit)
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to question_path
+  end
+
+  private
+  def set_question
+    @question = Question.find params[:id]
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body, :user_id)
   end
 end
