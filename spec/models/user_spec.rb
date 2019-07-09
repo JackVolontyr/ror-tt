78755d6a0aa9d3b8aadc63af054c148474aaa63e
JackVolontyr <soapforwork@gmail.com>
2019-07-09 18:46:56 +0300

    Update `spec/factories/users`, create `user_spec`.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
+
+  describe 'associations' do
+    it { should have_many(:questions).class_name('Question') }
+  end
+end
