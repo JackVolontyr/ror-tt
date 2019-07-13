5ad2369e1e87b97af0c11427a7090f1c6f28f1fa
JackVolontyr <soapforwork@gmail.com>
2019-07-13 16:23:32 +0300

    Create `answer_spec` for answer model, update another model's specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:questions).class_name('Question') }
+    it { should have_many(:answers).class_name('Answer') }
+  end
+
   it { should validate_presence_of :name }
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
-
-  describe 'associations' do
-    it { should have_many(:questions).class_name('Question') }
-  end
 end
