72bbdb39464a4bc8ea16cabcecb5d8d111adfde3
JackVolontyr <soapforwork@gmail.com>
2019-07-13 20:13:50 +0300

    Create `create_answer_spec`, update selector in `spec/acceptance`.

---
+++ spec/acceptance/create_answer_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+feature 'User create answer', %q{
+  In order to exchange my knowledge
+  As an (authenticated) user
+  I want to be able to create an answer
+} do
+
+  given(:user) { create :user }
+  given(:question) { create :question }
+
+  before {
+    @answer_show_button_selector ='[data-spec="answer_show_form"]'
+    @answer_submit_button_selector = '[data-spec="answer_submit"]'
+    @new_answer = 'It is body answer.'
+    @answer_container_selector = '[data-spec="answer_container"]'
+
+    @question_submit_button_selector = '[data-spec="question_submit"]'
+    @new_question_title = 'Title'
+    @new_question_body = 'Body body.'
+  }
+
+  scenario 'User create answer for the question' do
+    sign_in user
+
+    fill_in t_from(:new_question, 'title'), with: @new_question_title
+    fill_in t_from(:new_question, 'body'), with: @new_question_body
+    find(@question_submit_button_selector).click
+
+    find(@answer_show_button_selector).click
+    fill_in t_from(:new_answer, 'body'), with: @new_answer
+    find(@answer_submit_button_selector).click
+
+    expect(current_path).to eq welcomes_path
+    expect(page).to have_content t_from(:new_answer, 'success')
+    within @answer_container_selector do
+      expect(page).to have_content @new_answer
+    end
+  end
+
+  scenario 'User try create answer with invalid params'
+end
--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -9,7 +9,7 @@ feature 'User create Question', %q{
   given(:user) { create :user }
 
   before {
-    @submit_button_selector = '[data-spec="question_submit"]'
+    @question_submit_button_selector = '[data-spec="question_submit"]'
     @new_question_title = 'Title'
     @new_question_body = 'Body body.'
     @invalid_question_title = ''
@@ -20,7 +20,7 @@ feature 'User create Question', %q{
 
     fill_in t_from(:new_question, 'title'), with: @new_question_title
     fill_in t_from(:new_question, 'body'), with: @new_question_body
-    find(@submit_button_selector).click
+    find(@question_submit_button_selector).click
 
     expect(page).to have_content t_from(:new_question, 'success')
     expect(current_path).to eq welcomes_path
@@ -31,7 +31,7 @@ feature 'User create Question', %q{
 
     fill_in t_from(:new_question, 'title'), with: @invalid_question_title
     fill_in t_from(:new_question, 'body'), with: @new_question_body
-    find(@submit_button_selector).click
+    find(@question_submit_button_selector).click
 
     expect(page).to have_content t_from(:errors, 'blank')
     expect(current_path).to eq welcomes_path
@@ -42,7 +42,7 @@ feature 'User create Question', %q{
 
     fill_in t_from(:new_question, 'title'), with: @new_question_title
     fill_in t_from(:new_question, 'body'), with: @new_question_body
-    find(@submit_button_selector).click
+    find(@question_submit_button_selector).click
 
     expect(page).to have_content t_from(:failure, 'unauthenticated')
     expect(current_path).to eq new_user_session_path
