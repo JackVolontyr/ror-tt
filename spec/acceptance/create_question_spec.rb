5ac8f98bc4e71a0dcf0715c3e1d96159104f9f5c
JackVolontyr <soapforwork@gmail.com>
2019-07-13 12:21:52 +0300

    Update `create_question_spec`, add button selector to create question form.

--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -3,21 +3,23 @@ require 'rails_helper'
 feature 'User create Question', %q{
   In order to get answer from community
   As an (authenticated) user
-  I want to be able to ask question
+  I want to be able to ask the question
 } do
 
   given(:user) { create :user }
 
   before {
-    @submit_button_selector =
-        "input[type='submit'][value='#{t_from :new_question, 'submit'}']"
+    @submit_button_selector = '[data-spec="question_submit"]'
+    @new_question_title = 'Title'
+    @new_question_body = 'Body body.'
+    @invalid_question_title = ''
   }
 
   scenario 'Registered user try to create question' do
     sign_in user
 
-    fill_in t_from(:new_question, 'title'), with: 'Title'
-    fill_in t_from(:new_question, 'body'), with: 'Body body.'
+    fill_in t_from(:new_question, 'title'), with: @new_question_title
+    fill_in t_from(:new_question, 'body'), with: @new_question_body
     find(@submit_button_selector).click
 
     expect(page).to have_content t_from(:new_question, 'success')
@@ -27,8 +29,8 @@ feature 'User create Question', %q{
   scenario 'Registered user try to create question with invalid params' do
     sign_in user
 
-    fill_in t_from(:new_question, 'title'), with: ''
-    fill_in t_from(:new_question, 'body'), with: 'Body body.'
+    fill_in t_from(:new_question, 'title'), with: @invalid_question_title
+    fill_in t_from(:new_question, 'body'), with: @new_question_body
     find(@submit_button_selector).click
 
     expect(page).to have_content t_from(:errors, 'blank')
@@ -38,8 +40,8 @@ feature 'User create Question', %q{
   scenario 'Non-registered user try to sign in' do
     visit welcomes_path
 
-    fill_in t_from(:new_question, 'title'), with: 'Title'
-    fill_in t_from(:new_question, 'body'), with: 'Body body.'
+    fill_in t_from(:new_question, 'title'), with: @new_question_title
+    fill_in t_from(:new_question, 'body'), with: @new_question_body
     find(@submit_button_selector).click
 
     expect(page).to have_content t_from(:failure, 'unauthenticated')
