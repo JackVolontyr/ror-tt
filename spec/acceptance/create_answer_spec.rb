bc68d1d8007bc533e3c6d0dc34438e35458a4a79
JackVolontyr <soapforwork@gmail.com>
2019-07-15 19:32:41 +0300

    Set new logic of helper (`acceptance_helper`). Update `rails_helper`. Small update of `acceptance/.._spec`.

--- spec/acceptance/create_answer_spec.rb
+++ spec/acceptance/create_answer_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative 'acceptance_helper'
 
 feature 'User create answer', %q{
   In order to exchange my knowledge
@@ -10,28 +10,28 @@ feature 'User create answer', %q{
   given(:question) { create :question }
 
   before {
-    @answer_show_button_selector ='[data-spec="answer_show_form"]'
-    @answer_submit_button_selector = '[data-spec="answer_submit"]'
+    @answer_show_selector ='[data-spec="answer_show_form"]'
+    @answer_submit_selector = '[data-spec="answer_submit"]'
     @new_answer = 'It is body answer.'
     @answer_container_selector = '[data-spec="answer_container"]'
     @invalid_answer_title = ''
 
-    @question_submit_button_selector = '[data-spec="question_submit"]'
+    @question_submit_selector = '[data-spec="question_submit"]'
     @new_question_title = 'Title'
     @new_question_body = 'Body body.'
 
-    sign_in user
+    log_in user
 
     fill_in t_from(:new_question, 'title'), with: @new_question_title
     fill_in t_from(:new_question, 'body'), with: @new_question_body
-    find(@question_submit_button_selector).click
+    find(@question_submit_selector).click
 
-    find(@answer_show_button_selector).click
+    find(@answer_show_selector).click
   }
 
   scenario 'User create answer for the question', js: true do
     fill_in t_from(:new_answer, 'body'), with: @new_answer
-    find(@answer_submit_button_selector).click
+    find(@answer_submit_selector).click
 
     expect(current_path).to eq welcomes_path
     within @answer_container_selector do
@@ -41,7 +41,7 @@ feature 'User create answer', %q{
 
   scenario 'User try create answer with invalid params', js: true do
     fill_in t_from(:new_answer, 'body'), with: @invalid_answer_title
-    find(@answer_submit_button_selector).click
+    find(@answer_submit_selector).click
 
     expect(current_path).to eq welcomes_path
   end
--- spec/acceptance/sing_in_spec.rb
+++ spec/acceptance/sing_in_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative 'acceptance_helper'
 
 feature 'User sign in', %q{
   In order to be able to ask question
@@ -9,13 +9,13 @@ feature 'User sign in', %q{
   given(:user) { create :user }
 
   scenario 'Registered user try to sign in' do
-    sign_in user
+    log_in user
     expect(page).to have_content t_from(:sessions, 'signed_in')
     expect(current_path).to eq welcomes_path
   end
 
   scenario 'Non-registered user try to sign in' do
-    sign_in user, false
+    log_in user, false
     expect(page).to have_content t_from(:failure, 'invalid_email')
     expect(current_path).to eq new_user_session_path
   end
