2b719b7161cef9863c8134ecd598d55d59366634
JackVolontyr <soapforwork@gmail.com>
2019-07-17 11:29:30 +0300

    Update support macroses.

--- spec/support/capybara_macros.rb
+++ spec/support/capybara_macros.rb
@@ -1,10 +1,10 @@
 require 'webdrivers'
-# require 'selenium-webdriver'
+require 'selenium-webdriver'
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
-#
+
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: %w(headless window-size=1024,768) }, loggingPrefs: { browser: 'ALL' })
   Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
