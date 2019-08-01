c80669c8ca692239e668ebb6f67acd7066734f86
JackVolontyr <soapforwork@gmail.com>
2019-08-01 16:50:25 +0300

    Update spec for new attachment_spec.

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -33,6 +33,10 @@ RSpec.describe QuestionsController, type: :controller do
     it 'assigns a new Question to @question' do
       expect(assigns(:question)).to be_a_new Question
     end
+
+    it 'build new Attachment to @question' do
+      expect(assigns(:question).attachments.last).to be_a_new Attachment
+    end
   end
 
   describe 'GET #edit' do
@@ -41,6 +45,10 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'assigns the requested question to @question' do
       expect(assigns(:question)).to eq question
+    end
+
+    it 'build new Attachment to @question' do
+      expect(assigns(:question).attachments.last).to be_a_new Attachment
     end
   end
 
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe Question, type: :model do
   describe 'associations' do
     it { should belong_to(:user).class_name('User') }
     it { should have_many(:answers).class_name('Answer') }
+    it { should have_many(:attachments).class_name('Attachment') }
   end
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
   it { should validate_presence_of :user }
+
+  it { should accept_nested_attributes_for :attachments }
 end
