89b89a4531585b2bc82d59e5eb3e70610180c747
JackVolontyr <soapforwork@gmail.com>
2019-08-02 18:03:11 +0300

    Add polymorphic to models and accepts_nested_attributes for :attachments to answer.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,9 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
+  has_many :attachments, as: :attachmentable, dependent: :destroy
 
   validates :body, :question, :user, presence: true
+
+  accepts_nested_attributes_for :attachments, allow_destroy: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
-  has_many :attachments, dependent: :destroy, inverse_of: :question
+  has_many :attachments, as: :attachmentable, dependent: :destroy
 
   validates :title, :body, :user, presence: true
 
