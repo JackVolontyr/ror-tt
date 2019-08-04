78565f31cdc66f7a1d6084d6e1e2bc73f9f7e0af
JackVolontyr <soapforwork@gmail.com>
2019-08-04 15:04:52 +0300

    Change response format from js to html. Add TODO for attachments.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,7 +6,18 @@ class AnswersController < ApplicationController
   def create
     @answer = @question.answers.build answer_params
     @answer.user = current_user
-    @answer.save
+    respond_to do |format|
+      if @answer.save
+        format.html { render partial: 'answers/answers', layout: false }
+        # format.json { render json: @answer }
+      else
+        format.html { render partial: 'answers/error_explanation',
+                             status: :unprocessable_entity, # 422
+                             layout: false }
+        # format.json { render json: @answer.errors.full_messages,
+        #                      status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
